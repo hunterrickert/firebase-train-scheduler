26be3024c448e5f247c34c7f30c8a85fc0a3d437
Hunter Rickert <hunter.rickert@gmail.com>
2019-08-01 21:34:55 -0700

    final js

--- app.js
+++ app.js
@@ -21,3 +21,5 @@
 //           clickCounter: clickNumber
 //       });
 //   });
+
+    
--- train-schedule.js
+++ train-schedule.js
@@ -6,35 +6,35 @@ var firebaseConfig = {
     storageBucket: "",
     messagingSenderId: "923461931032",
     appId: "1:923461931032:web:db1ca4532f7e9e2a"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 let database = firebase.database();
 
-$(document).on("click", ".search", function() {
-    let splitDate = $(".startDate").val().split("/");
-    pushToDataBase($(".name").val(), $(".role").val(), new Date(parseInt(splitDate[2]), parseInt(splitDate[0]) - 1, parseInt(splitDate[1])), $(".rate").val());
-    $(".name").val("");
-    $(".role").val("");
-    $(".startDate").val("");
-    $(".rate").val("");
-});
+// $(document).on("click", ".search", function() {
+//     let splitDate = $(".startDate").val().split("/");
+//     pushToDataBase($(".name").val(), $(".role").val(), new Date(parseInt(splitDate[2]), parseInt(splitDate[0]) - 1, parseInt(splitDate[1])), $(".rate").val());
+//     $(".name").val("");
+//     $(".role").val("");
+//     $(".startDate").val("");
+//     $(".rate").val("");
+// });
 
-database.ref().on("child_added", function(snapshot) {
+database.ref().on("child_added", function (snapshot) {
     displayEmployees(snapshot.val());
 });
 
 function displayEmployees(employee) {
     console.log(employee.employee);
     let $tableRow = $("<tr>");
-    let $dataName = $("<td>").html(employee.employee.name);
-    let $dataRole = $("<td>").html(employee.employee.role);
-    let $dataStartDate = $("<td>").html(employee.employee.startDate);
-    let $dataMonths = $("<td>").html(calculateMonthsWorked(employee.employee.secondsWorked));
-    let $dataRate = $("<td>").html(employee.employee.rate);
-    let $dataTotalIncome = $("<td>").html(calculatePaid(employee.employee.rate, calculateMonthsWorked(employee.employee.secondsWorked)));
-    $tableRow.append($dataName, $dataRole, $dataStartDate, $dataMonths, $dataRate, $dataTotalIncome, $("<hr>"));
+    let $dataTrainName = $("<td>").html(employee.employee.name);
+    let $dataDestination = $("<td>").html(employee.employee.role);
+    let $dataFrequency = $("<td>").html(employee.employee.startDate);
+    let $dataArrival = $("<td>").html(calculateNextArrivals(employee.employee.secondsWorked));
+    // let $dataRate = $("<td>").html(employee.employee.rate);
+    let $dataMinAway = $("<td>").html(calculateMinAway(employee.employee.rate, calculateNextArrival(employee.employee.secondsWorked)));
+    $tableRow.append($dataTrainName, $dataDestination, $dataFrequency, $dataArrival, $dataMinAway, $("<hr>"));
     $(".employees").append($tableRow);
 }
 
@@ -52,10 +52,20 @@ function pushToDataBase(name, role, startDate, rate) {
 }
 
 // Returns months from seconds
-function calculateMonthsWorked(seconds) {
-    return Math.round(seconds / 60 / 60 / 24 / 30);
-}
+// function calculateMonthsWorked(seconds) {
+//     return Math.round(seconds / 60 / 60 / 24 / 30);
+// }
 
-function calculatePaid(rate, months) {
-    return Math.round(rate * months);
-}
+// function calculatePaid(rate, months) {
+//     return Math.round(rate * months);
+// }
+var currentTime = moment();
+console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+var firstTime = moment($("#firstTime").val().trim(), "hh:mm").subtract(1, "years").format("X");
+
+var frequency = $("#freq").val().trim();
+
+//current time
+var currentTime = moment();
+console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
