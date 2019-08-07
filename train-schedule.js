c0632da9347c672423efa79c1cf32ada81050008
Hunter Rickert <hunter.rickert@gmail.com>
2019-08-06 19:02:14 -0700

    getting user input to display inresults

--- train-schedule.js
+++ train-schedule.js
@@ -15,41 +15,73 @@ let database = firebase.database();
 // $(document).on("click", ".search", function() {
 //     let splitDate = $(".startDate").val().split("/");
 //     pushToDataBase($(".name").val(), $(".role").val(), new Date(parseInt(splitDate[2]), parseInt(splitDate[0]) - 1, parseInt(splitDate[1])), $(".rate").val());
-//     $(".name").val("");
-//     $(".role").val("");
+//     $(".trainName").val("");
+//     $(".").val("");
 //     $(".startDate").val("");
 //     $(".rate").val("");
 // });
 
-database.ref().on("child_added", function (snapshot) {
-    displayEmployees(snapshot.val());
-});
+// button for adding trains
+$(".add-train-button").on("click", function (event) {
+    event.preventDefault();
 
-function displayEmployees(employee) {
-    console.log(employee.employee);
-    let $tableRow = $("<tr>");
-    let $dataTrainName = $("<td>").html(employee.employee.name);
-    let $dataDestination = $("<td>").html(employee.employee.role);
-    let $dataFrequency = $("<td>").html(employee.employee.startDate);
-    let $dataArrival = $("<td>").html(calculateNextArrivals(employee.employee.secondsWorked));
-    // let $dataRate = $("<td>").html(employee.employee.rate);
-    let $dataMinAway = $("<td>").html(calculateMinAway(employee.employee.rate, calculateNextArrival(employee.employee.secondsWorked)));
-    $tableRow.append($dataTrainName, $dataDestination, $dataFrequency, $dataArrival, $dataMinAway, $("<hr>"));
-    $(".employees").append($tableRow);
-}
+    //grabs user input
+    var trainName = $(".trainName")
+        .val()
+        .trim();
+    var destination = $(".destination")
+        .val()
+        .trim();
+    var firstTime = moment(
+        $(".firstTime")
+            .val()
+            .trim(),
+        "HH:mm"
+    ).format("X");
+    var freq = $(".freq")
+        .val()
+        .trim();
 
-function pushToDataBase(name, role, startDate, rate) {
-    database.ref().push({
-        employee: {
-            name: name,
-            role: role,
-            startDate: startDate.toDateString(),
-            secondsWorked: (new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDay()).getTime() - startDate.getTime()) / 1000,
-            rate: rate
-        }
-    });
 
-}
+    //create local "temporary" objects for holding employee data
+    var newTrain = {
+        train: trainName,
+        dest: destination,
+        time: firstTime,
+        freq: freq,
+    }
+
+    console.log(newTrain.train);
+    console.log(newTrain.dest);
+    console.log(newTrain.time);
+    console.log(newTrain.freq);
+
+    alert("Train successfully added");
+
+    $(".trainName").val("");
+    $(".destination").val("");
+    $(".firstTime").val("");
+    $(".freq").val("");
+
+
+    var nextArrival = 
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        // $("<td>").text(nextArrival),
+        // $("<td>").text(minAway),
+    );
+    
+    $(".results-titles > tbody").append(newRow);
+})
+
+// var nextArrival = 
+
+
+
+
 
 // Returns months from seconds
 // function calculateMonthsWorked(seconds) {
@@ -59,13 +91,48 @@ function pushToDataBase(name, role, startDate, rate) {
 // function calculatePaid(rate, months) {
 //     return Math.round(rate * months);
 // }
+
+
+
+
 var currentTime = moment();
 console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-var firstTime = moment($("#firstTime").val().trim(), "hh:mm").subtract(1, "years").format("X");
+var firstTime = moment($("#firstTime").val(), "hh:mm").subtract(1, "years").format("X");
 
-var frequency = $("#freq").val().trim();
+var frequency = $("#freq").val();
 
 //current time
 var currentTime = moment();
 console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+
+// database.ref().on("child_added", function (snapshot) {
+//     displayTrains(snapshot.val());
+// });
+
+// function displayTrains(trains) {
+//     console.log(train.train);
+//     let $tableRow = $("<tr>");
+//     let $dataTrainName = $("<td>").html(train.train.name);
+//     let $dataDestination = $("<td>").html(train.train.role);
+//     let $dataFrequency = $("<td>").html(train.train.startDate);
+//     let $dataArrival = $("<td>").html(calculateNextArrivals(train.train.minutesArrival));
+//     // let $dataRate = $("<td>").html(employee.employee.rate);
+//     let $dataMinAway = $("<td>").html(calculateMinAway(employee.employee.rate, calculateNextArrival(employee.employee.secondsWorked)));
+//     $tableRow.append($dataTrainName, $dataDestination, $dataFrequency, $dataArrival, $dataMinAway, $("<hr>"));
+//     $(".employees").append($tableRow);
+// }
+
+// function pushToDataBase(name, role, startDate, rate) {
+//     database.ref().push({
+//         employee: {
+//             name: name,
+//             role: role,
+//             startDate: startDate.toDateString(),
+//             secondsWorked: (new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDay()).getTime() - startDate.getTime()) / 1000,
+//             rate: rate
+//         }
+//     });
+
+// }
